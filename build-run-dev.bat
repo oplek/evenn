e2e63a1f1a6756f8a5a10505b4dd6e68adf5c451
Jasper Turcotte <jasper.turcotte@gmail.com>
2021-04-25 12:07:58 -0400

    Foundation updates 2

--- build-run-dev.bat
+++ build-run-dev.bat
@@ -1,4 +1,5 @@
 docker build --tag evenn:latest -f docker/Dockerfile-dev .
 pause
-docker run -d -t -p 8080:80 -e DB_NAME=main -e DB_PASSWD=1234 -v ./webroot:/var/www/html/webroot evenn:latest
+rem docker run -d -it -p 8080:80 -e DB_NAME=main -e DB_PASSWD=1234 -v ./webroot:/var/www/html/webroot evenn:latest
+docker run -d -it -p 8080:80 -v ./webroot:/var/www/html/webroot evenn:latest
 pause
